81eb690e31f2e7eaf6b5a1bc03937490def10b20
Christian Høj <cbh@ucn.dk>
2013-10-16 14:20:14 +0200

    Initial commit

---
+++ Forester/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Forester
+//
+//  Created by Christian Høj on 16/10/13.
+//  Copyright (c) 2013 Christian Høj. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
